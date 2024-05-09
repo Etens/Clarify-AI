757a50839238528599572e65a47545e3fd045bb5
Etens <45323562+Etens@users.noreply.github.com>
2024-05-09 22:30:23 +0200

    ⚙️ Enhance illustration search route to accurately handle keyword-based queries

--- app/api/illustrations/route.ts
+++ app/api/illustrations/route.ts
@@ -8,7 +8,7 @@ interface NextApiRequestWithUrl extends NextApiRequest {
 
 export async function GET(req: NextApiRequestWithUrl) {
   try {
-
+    const style = req.nextUrl.searchParams.get('style') || 'rafiki';
     const keyword = req.nextUrl.searchParams.get('keyword') || 'nature';
     if (!keyword) {
       return new Response(JSON.stringify({ message: "Missing keyword parameter" }), {
@@ -19,7 +19,7 @@ export async function GET(req: NextApiRequestWithUrl) {
       console.log('Récupération du mot-clé avec succès :', keyword);
     }
 
-    const url = 'https://storyset.com/rafiki';
+    const url = `https://storyset.com/illustration/${keyword}/${style}`;
     if (!url) {
       return new Response(JSON.stringify({ message: "Missing URL parameter" }), {
         status: 400,
@@ -30,63 +30,49 @@ export async function GET(req: NextApiRequestWithUrl) {
     }
 
     const { data: html } = await axios.get(url);
-
     if (!html) {
       return new Response(JSON.stringify({ message: "No HTML content found" }), {
         status: 404,
         headers: { 'Content-Type': 'application/json' },
       });
     } else {
-      console.log('Récupération du HTML avec succès');
+      console.log('Contenu HTML récupéré avec succès :', html.slice(0, 100));
     }
 
     const $ = cheerio.load(html);
-    if (!$) {
-      return new Response(JSON.stringify({ message: "No HTML content found" }), {
-        status: 404,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    } else {
-      console.log('Chargement du HTML avec succès');
-    }
+    let imageUrl = null;
+    $('script[type="application/ld+json"]').each((_i, elem) => {
+      const scriptContent = $(elem).html() as string;
+      const keywordName = keyword.replace(/-/g, ' ');
+      try {
+        const jsonData = JSON.parse(scriptContent);
+        if (jsonData['@type'] === 'ImageObject' &&
+          jsonData.name.toLowerCase().replace(/-/g, ' ') === keywordName.toLowerCase() &&
+          jsonData.thumbnailUrl.includes(style)) {
+          imageUrl = jsonData.thumbnailUrl.replace(/"/g, '');
+        }
+      } catch (error) {
+        console.error("Erreur lors du parsing JSON :", error);
+      }
+    });
 
-    console.log('Recherche du mot-clé dans le HTML...');
-    const parent = $(`a[title="${keyword}"]`).html();
-    if (!parent) {
-      return new Response(JSON.stringify({ message: "Keyword not found" }), {
-        status: 404,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    } else {
-      console.log('Mot-clé trouvé avec succès :', parent);
-    }
-    const script: string | null = $(parent).find('script').html();
-    if (!script) {
-      return new Response(JSON.stringify({ message: "No script found" }), {
-        status: 404,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    } else {
-      console.log('Script trouvé avec succès :', script);
-    }
-    const scriptJson = JSON.parse(script);
-    console.log('Script parsé avec succès :', scriptJson);
-    const imageUrl = scriptJson.thumbnailUrl;
-    if (!imageUrl) {
-      return new Response(JSON.stringify({ message: "No image found" }), {
-        status: 404,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    } else {
-      return new Response(JSON.stringify({ imageUrl }), {
+    if (imageUrl) {
+      console.log('URL de l\'image récupérée avec succès :', imageUrl);
+      return new Response(imageUrl, {
         status: 200,
+        headers: { 'Content-Type': 'text/plain' },
+      });
+    } else {
+      return new Response(JSON.stringify({ message: "No image URL found" }), {
+        status: 404,
         headers: { 'Content-Type': 'application/json' },
       });
     }
+  }
 
-  } catch (error) {
-    console.error('Erreur lors du scraping:', error);
-    return new Response(JSON.stringify({ message: "Erreur interne du serveur" }), {
+  catch (error) {
+    console.error("Erreur lors de la récupération des données");
+    return new Response(JSON.stringify({ message: "Internal server error" }), {
       status: 500,
       headers: { 'Content-Type': 'application/json' },
     });
